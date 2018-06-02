85b2793f5a3b37a29c1e98a7c9d8b785ee1375d1
nataliercraun <craun@usc.edu>
2018-06-02 10:21:18 -0500

    Add try catch

--- server.js
+++ server.js
@@ -25,25 +25,29 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', async (req, res) => {
-	var message = new Message(req.body)
+	try {
+		var message = new Message(req.body)
 
-	var savedMessage = await message.save()
+		var savedMessage = await message.save()
 
-	console.log('saved')
-	
-	var censored = await Message.findOne({message: 'badword'})
+		console.log('saved')
+		
+		var censored = await Message.findOne({message: 'badword'})
 
-	if (censored) 
-		await Message.remove({_id: censored.id})
-	else 
-		io.emit('message', req.body)
+		if (censored) 
+			await Message.remove({_id: censored.id})
+		else 
+			io.emit('message', req.body)
 
-	res.sendStatus(200)
-
-		//.catch((err) => {
-			//	res.sendStatus(500)
-			//	return console.error(err)
-		//})	
+		res.sendStatus(200)
+	} catch (error) {
+		res.sendStatus(500)
+		return console.error(error)
+	} finally {
+		//logger.log('message post called')
+		// close DB connection 
+		console.log('message post called')
+	}	
 })
 
 io.on('connection', (socket) => {
